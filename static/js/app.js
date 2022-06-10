9a2f76fe32a50f0e7ff3dd2e18157bfed2c3c649
Adriana Hernandez <adrianahe@lstc.lesschwab.com>
2022-06-10 10:43:00 -0700

    updates

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+//Build table
+function buildTable(data) {
+    tbody.html("");
+}
+//Loop through each object in the data and appenda row and cells for each value in the row
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    //loop through each field in the dataRow and add each value as a table cell
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+});
+}
